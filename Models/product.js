a65c7626004f127d28274261f69d8c3607d40165
veerabv <bveera0608@gmail.com>
2024-07-26 11:32:33 +0530

    implement the calbacklogic to the issue

--- Models/product.js
+++ Models/product.js
@@ -13,14 +13,14 @@ module.exports = class Product{    //cretae a class to work with datas
     }
     save(){   // this will be the method of class and will be new to all new instance of the class
         const p = path.join(rootDir , 'data','product.json');
-        fs.readFile(p ,(err , fileContent) => {
+        fs.readFile(p ,(err , fileContent) => {    // this will read thw file and update
             // console.log(err);
             let product = [];
             if(!err){
               product =   JSON.parse(fileContent)
             }
             product.push(this);
-            fs.writeFile(p , JSON.stringify(product),(err)=>{
+            fs.writeFile(p , JSON.stringify(product),(err)=>{  // store the updated data in the file
                 console.log(err);
             })
         })
@@ -29,14 +29,14 @@ module.exports = class Product{    //cretae a class to work with datas
 
     }
 
-    static fetchAllProduct(){  // the static method will be called directly from the class 
+    static fetchAllProduct(cb){  // the static method will be called directly from the class 
         const p = path.join(rootDir , 'data','product.json');
-      fs.readFile(p , (err,fileContent)=>{
-        
+      fs.readFile(p , (err,fileContent)=>{  // get the data form the file , there is a issue in this is the return is only for the inner function . when the fs.readFile executes the function gets over and register the calbback function (err , fileContent)
+         
             if(err){
-                return []
+                cb([])   
             }
-            return JSON.stringify(fileContent)
+            cb(JSON.parse(fileContent))
         })
         
     }
--- controllers/products.js
+++ controllers/products.js
@@ -1,26 +1,24 @@
-const Product = require('../Models/product');  // we import the class Product
-
-
+const Product = require("../Models/product"); // we import the class Product
 
 exports.getAddProducts = (req, res, next) => {
-    res.render('add-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-  
-    });
-}
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
+};
 
 exports.postAddProducts = (req, res, next) => {
-    const product = new Product(req.body.title)  // create a new instance
-    product.save() // run the method from the instance
-    res.redirect('/');
-  };
+  const product = new Product(req.body.title); // create a new instance
+  product.save(); // run the method from the instance
+  res.redirect("/");
+};
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAllProduct();  // run the static method from the Class name itself "Product" capital P
+  Product.fetchAllProduct((products) =>
     res.render("shop", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
-    });
-  }
+    })
+  ); // run the static method from the Class name itself "Product" capital P
+};
