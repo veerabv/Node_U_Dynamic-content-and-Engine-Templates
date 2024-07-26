dc92a0283731a21296177986f8f178b2aed09c4d
veerabv <bveera0608@gmail.com>
2024-07-26 15:56:45 +0530

    add riouting link to the Details button

--- Models/product.js
+++ Models/product.js
@@ -24,6 +24,7 @@ module.exports = class Product {
     
   }
   save() {
+    this.id = Math.random().toString();
     getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
