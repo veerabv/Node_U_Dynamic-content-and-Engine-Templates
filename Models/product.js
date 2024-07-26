ecaa5076bc0fcb8887bae44451d4bed110d604d5
veerabv <bveera0608@gmail.com>
2024-07-26 11:11:21 +0530

    store the data in a file using fs Readfile and writefile , there is a issue related to js working see the comment

--- Models/product.js
+++ Models/product.js
@@ -1,13 +1,43 @@
+const rootDir = require("../util/path");
+
+const path = require("path");
+const fs =require("fs");
+
+
+
+
 const products = []
 module.exports = class Product{    //cretae a class to work with datas
     constructor(title){
         this.title =title;
     }
     save(){   // this will be the method of class and will be new to all new instance of the class
-        products.push(this)  
+        const p = path.join(rootDir , 'data','product.json');
+        fs.readFile(p ,(err , fileContent) => {
+            // console.log(err);
+            let product = [];
+            if(!err){
+              product =   JSON.parse(fileContent)
+            }
+            product.push(this);
+            fs.writeFile(p , JSON.stringify(product),(err)=>{
+                console.log(err);
+            })
+        })
+
+
+
     }
 
     static fetchAllProduct(){  // the static method will be called directly from the class 
-        return products;
+        const p = path.join(rootDir , 'data','product.json');
+      fs.readFile(p , (err,fileContent)=>{
+        
+            if(err){
+                return []
+            }
+            return JSON.stringify(fileContent)
+        })
+        
     }
 }
