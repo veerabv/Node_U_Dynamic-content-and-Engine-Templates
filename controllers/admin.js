1c36592cc93ec4624041808c2cdff3972f437c28
veerabv <bveera0608@gmail.com>
2024-07-31 21:04:03 +0530

    fetching the data depends on the user

--- controllers/admin.js
+++ controllers/admin.js
@@ -26,20 +26,24 @@ exports.postAddProducts = (req, res, next) => {
   //   .then((product) => res.redirect("/admin/products"))
   //   .catch((err) => console.log(err));
 
-    //  this is anotherWay elequent way
+  //  this is anotherWay elequent way
 
-    req.user.createProduct({    // we set the user in app.js middlewhere refer the readme file to know about where we get this "createProduct" suddenly  . the  Product in createProduct is come from the model name we gave
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
+  req.user
+    .createProduct({
+      // we set the user in app.js middlewhere refer the readme file to know about where we get this "createProduct" suddenly  . the  Product in createProduct is come from the model name we gave
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
     })
     .then((product) => res.redirect("/admin/products"))
     .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  req.user
+    .getProducts({ where: { userId: req.user.id } }) //change this to the particular logged in admin user . display only thr products related to that admin not all products
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -56,13 +60,14 @@ exports.getEditProduct = (req, res, next) => {
   const productId = req.params.productId;
 
   Product.findByPk(productId)
+
     .then((product) => {
       // here we will find the product and prepopulated in the product form
       res.render("admin/edit-product", {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: isEdit,
-        product,
+        product: product,
       });
     })
     .catch((err) => console.log(err));
@@ -90,10 +95,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.id;
-  Product.findByPk(prodId)    
+  Product.findByPk(prodId)
     .then((product) => {
       // console.log(product, "?????????????????????????");
-      return product.destroy();  // find the product using id and use destroy method to delte the method
+      return product.destroy(); // find the product using id and use destroy method to delte the method
     })
     .then((prod) => {
       res.redirect("/admin/products");
